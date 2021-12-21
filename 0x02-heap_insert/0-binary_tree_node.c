9916127351bcdc5fbd64f5559b2e456d49435d0e
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2021-12-21 15:19:09 -0500

    tired today. I'll figure this out tomorrow.

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: pointer to the parent node
+ * @value: valaue of the new node
+ * Return: a pointer to the new node or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+
+}
---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * heap_insert - inserts a value into a max binary heap
+ * @root: double pointer to the root node of the heap
+ * @value: value to store in the node to be inserted
+ * Return: pointer to the inserted node, or NULL on failure
+ */
+
+heap_t *heap_insert(heap_t **root, int value) {
+    
+}
