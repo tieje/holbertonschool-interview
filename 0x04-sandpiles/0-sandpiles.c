326a72a516079f8d093acf7e10d6926473e0de2c
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-01-12 18:32:55 -0500

    sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,5 +1,26 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "sandpiles.h"
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
 /**
  * sandpiles_sum - computes the sum of two sandpiles
  * @grid1: grid 1
@@ -8,5 +29,39 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int i, j;
 
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+	while (grid1[0][0] > 3 || grid1[0][1] > 3 || grid1[0][2] > 3 ||
+		   grid1[1][0] > 3 || grid1[1][1] > 3 || grid1[1][2] > 3 ||
+		   grid1[2][0] > 3 || grid1[2][1] > 3 || grid1[2][2] > 3)
+	{
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+				temp[i][j] = grid1[i][j];
+		printf("=\n");
+		print_grid(grid1);
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					if (i - 1 >= 0)
+						temp[i - 1][j]++;
+					if (i + 1 < 3)
+						temp[i + 1][j]++;
+					if (j - 1 >= 0)
+						temp[i][j - 1]++;
+					if (j + 1 < 3)
+						temp[i][j + 1]++;
+					temp[i][j] -= 4;
+				}
+			}
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+				grid1[i][j] = temp[i][j];
+	}
 }
