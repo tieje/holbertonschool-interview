0f6d1a953ff66d6cf3ca7145a1eceba228c3cf8e
tieje <toj320@gmail.com>
2022-08-18 20:55:09 -0400

    wc: work in progress

---
+++ 0x21-substring/README.md
@@ -0,0 +1,1 @@
+# [0x21. Substring with concatenated words](https://intranet.hbtn.io/projects/506)
---
+++ 0x21-substring/substring.c
@@ -0,0 +1,20 @@
+#include "substring.h"
+
+
+/**
+ * find_substring - finds all possible substrings containing  a list of words
+ * @s: string to scan
+ * @words: is the array of words all substrings
+ * @nb_words: s the number of elements in the array words
+ * @n: address at which to store the number of elements in the returned array
+ * Return: an array storing each index in s where a substring was found,
+ * else NULL
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	(void)s;
+	(void)words;
+	(void)nb_words;
+	*n = 0;
+	return (NULL);
+}
